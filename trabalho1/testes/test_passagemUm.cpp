0ce72fb5a743a850e8afa6b1f3635896ceb9465a
makueba <makiander.abr@gmail.com>
2019-05-16 00:18:46 -0300

    added call for passagemUm for tests

--- trabalho1/testes/test_passagemUm.cpp
+++ trabalho1/testes/test_passagemUm.cpp
@@ -166,5 +166,52 @@ int main(){
     else
         cout<<"\33[1;31m verifyDir-Test 2 FAILED"<<endl;
 
+    //passagemUm test
+    map<string, int> pre =  {{"SECTION TEXT", 1},
+                            {"INPUT OLD_DATA", 2},
+                            {"LOAD OLD_DATA", 3},
+                            {"L1: DIV DOIS", 4},
+                            {"STORE NEW_DATA", 5},
+                            {"MULT DOIS", 6},
+                            {"STORE TMP_DATA", 7},
+                            {"load OLD_DATA", 8},
+                            {"SUB TMP_DATA", 9},
+                            {"STORE TMP_DATA", 10},
+                            {"OUTPUT TMP_DATA", 11},
+                            {"COPY NEW_DATA, OLD_DATA", 12},
+                            {"LOAD OLD_DATA", 13},
+                            {"STOP", 14},
+                            {"SECTION DATA", 15},
+                            {"DOIS: CONST 2", 16},
+                            {"OLD_DATA: SPACE 2", 17},
+                            {"NEW_DATA: SPACE", 18},
+                            {"TMP_DATA: SPACE", 19}};
+        
+    vector<string> program = {{"SECTION TEXT"},
+                            {"INPUT OLD_DATA"},
+                            {"LOAD OLD_DATA"},
+                            {"L1: DIV DOIS"},
+                            {"STORE NEW_DATA"},
+                            {"MULT DOIS"},
+                            {"STORE TMP_DATA"},
+                            {"load OLD_DATA"},
+                            {"SUB TMP_DATA"},
+                            {"STORE TMP_DATA"},
+                            {"OUTPUT TMP_DATA"},
+                            {"COPY NEW_DATALD_DATA"},
+                            {"LOAD OLD_DATA"},
+                            {"STOP"},
+                            {"SECTION DATA"},
+                            {"DOIS: CONST 2"},
+                            {"OLD_DATA: SPACE 2"},
+                            {"NEW_DATA: SPACE"},
+                            {"TMP_DATA: SPACE"}};
+    
+    passagemUm(pre,program);
+    cout<<"======================="<<endl;
+    map<string,int>::iterator it = simbolTable.begin();
+    for (it=simbolTable.begin(); it!=simbolTable.end(); ++it)
+    std::cout << it->first << " => " << it->second << '\n';
+
     return 0;
 }
